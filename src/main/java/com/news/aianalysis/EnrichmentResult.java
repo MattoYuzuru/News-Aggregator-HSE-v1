e87976a36c18699e09996ebac56f84efad6c9451
Matvei Riabushkin <matveybobr937@gmail.com>
2025-05-15 13:03:42 +0300

    Added empty method to EnrichementResult

--- src/main/java/com/news/aianalysis/EnrichmentResult.java
+++ src/main/java/com/news/aianalysis/EnrichmentResult.java
@@ -1,11 +1,21 @@
 package com.news.aianalysis;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.util.List;
 
+@Builder
+@Data
 public class EnrichmentResult {
     private String summary;
     private String region;
     private List<String> tags;
-
-    // геттеры/сеттеры
+    public static EnrichmentResult empty() {
+        return EnrichmentResult.builder()
+                .summary(null)
+                .region(null)
+                .tags(List.of())
+                .build();
+    }
 }
